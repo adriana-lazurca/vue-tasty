2c2956ae247d5e1c95f9cfc8d506de2f51a66d69
Adriana Lazurca <adriana.lazurca@gmail.com>
2021-05-11 02:17:53 +0200

    add new files

---
+++ src/components/Recipe.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="recipe-item ml-5">
+    <img class="recipe-item__image ml-5" :src="image.source" :alt="image.alt" />
+    <h4>{{ name }}</h4>
+    <ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Recipe",
+
+  props: {
+    image: {
+      type: Object,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    ingredients:{
+      type: Array,
+      required: true
+    },
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.recipe-item {
+  //width: 50%;
+  text-align: center;
+  //display: flex;
+}
+
+.recipe-item__image {
+  max-width: 50%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <b-row class="">
+      <b-col cols="6" v-for="recipe in recipes" :key="recipe.name">
+        <Recipe
+          :image="recipe.image"
+          :name="recipe.name"
+          :ingredients="recipe.ingredients"
+        />
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Recipe from "../components/Recipe.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    Recipe
+  },
+
+  data() {
+    return {
+      restaurantName: "TASTY",
+
+      recipes: [
+        {
+          name: "Banana bread",
+          image: {
+            source: "/images/bananaBread.png",
+            alt: "Banana bread"
+          },
+          ingredients: ["1 cup of sugar", "200ml of milk", "1 egg"]
+        },
+        {
+          name: "Cake",
+          image: {
+            source: "/images/bananaBread.png",
+            alt: "Banana bread"
+          },
+          ingredients: ["1 cup of sugar", "200ml of milk", "1 egg"]
+        },
+        {
+          name: "Bread",
+          image: {
+            source: "/images/bananaBread.png",
+            alt: "Banana bread"
+          },
+          ingredients: ["1 cup of sugar", "200ml of milk", "1 egg"]
+        },
+        {
+          name: "Crepes",
+          image: {
+            source: "/images/bananaBread.png",
+            alt: "Banana bread"
+          },
+          ingredients: ["1 cup of sugar", "200ml of milk", "1 egg"]
+        }
+      ]
+    };
   }
-}
+};
 </script>
+
+<style></style>
